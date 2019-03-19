d2a4635a757dc9276532011c9746d8e50cb2f954
Mar Bocatcat <mar.bocatcat@gmail.com>
2019-03-19 13:24:57 -0500

    replaced os.walk to using shutil.copytree

--- bars.py
+++ bars.py
@@ -1,10 +1,18 @@
 import os
-import shutil
 import logging
+from shutil import copytree, ignore_patterns
 
 # Set logging configurations.
 logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s = %(levelname)s \
                     - %(message)s')
+
+
+# Ignore Path patterns.
+def _logpath():
+    ignore = ['*.py', '*.so', '*.cfg', '*.h', '*.exe', '*.csh', '*.fish',
+     '*.js', '*.css', '*.json', '*.ps1', '*.pyo', '*.xml', '*.rst', '*.c', '*.tmpl']
+    for patterns in ignore:
+        return patterns
 
 
 logging.debug('Start of the loop.')
@@ -14,25 +22,31 @@ def print_targets():
     # Set HOME environment variable /home/bobcat.
     Home = os.getenv('HOME')
     # Put target directories in a list.
-    target = ['Desktop', 'Documents'] #, 'Documents', 'Downloads', 'Projects', 'Pictures']
-    # Ignore the following extensions.
-    ignore = ['.py', '.pyc', '.so', '.cfg', '.h', '.exe', '.csh', '.fish',
-     '.js', '.css', '.json', '.ps1', '.pyo', '.xml', '.rst', '.c', '.tmpl']
+    target = ['Desktop', 'Documents', 'Downloads', 'Projects', 'Pictures']
 
     # Loop starts.
     for targets in target:
-        # Create target path by concatenating the home directory and target /
-        # dirs.
-        dir_path = f'{Home}/{targets}'
-        for root, dir, file in os.walk(dir_path):
-            for entries in file:
-                if not entries.endswith(tuple(ignore)):
-                    destination = f'{Home}/bars'
-                    print(f'Copying {entries} to {destination}')
-                # source = os.path.join(root, entries)
-                # print(f'Copying {source} to {destination}')
-                # logging.debug('Copying ' + shutil.copy(source, destination) + f' to {destination}')
-    logging.debug('End of the loop.')
+        # Create target and destination path.
+        destination = f'{Home}/bars/{targets}'
+        targets = f'{Home}/{targets}'
+        # Add the logging and copytree ignore patterns.
+        # Main logic is here.
+        logging.debug(f'Copying {copytree(targets, destination, ignore=ignore_patterns(_logpath()))}')
+    else:
+        # Raise this exception if error occured.
+        raise Exception('An Error Occured!')
 
 
-print_targets()
+logging.debug('End of the loop.')
+
+
+def main():
+    try:
+        print_targets()
+    # Grab exception from the Raise on the print_targets loop.
+    except Exception as err:
+        print('An exception happend: ' + str(err))
+
+
+# Run main.
+main()
